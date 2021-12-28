e06d76574e774c2d3dc9f3101e67415b338ed3b7
baimamboukar <baimamboukar@gmail.com>
2021-12-28 14:53:27 +0100

    Driver code

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Driver code that display all file name inside src directory and run one file
+     * depending on user input
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the file name: ");
+        String fileName = input.nextLine();
+        input.close();
+        try {
+            Class<?> cls = Class.forName("src." + fileName);
+            cls.getMethod("main", String[].class).invoke(null, (Object) new String[] {});
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+    }
+
+}
